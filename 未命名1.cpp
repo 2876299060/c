9eda9c593e71f95bbe65a9251c21fcf4cf8a7071
孙丹阳 <151449361+2876299060@users.noreply.github.com>
2023-11-25 10:02:11 +0800

    Add files via upload

---
+++ 未命名1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int line=0;
+	printf("进入大学\n");
+	
+	while(line<20)
+	{printf("敲代码%d\n",line);
+	 line++;
+	}
+	if(line>=20)
+	printf("得到好offeer\n");
+	return 0;
+}
