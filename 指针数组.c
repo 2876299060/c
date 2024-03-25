996a2cf86d27450255885e0cfbdb5e15a96acbff
孙丹阳 <151449361+2876299060@users.noreply.github.com>
2024-03-25 15:44:22 +0800

    Add files via upload

---
+++ 指针数组.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a=1;
+	int b=2;
+	int c=3;
+	int d=4;
+	int i=0;
+	int* arr[4]={&a,&b,&c,&d};
+	for(i=0;i<4;i++)
+	{
+		printf("%d\n",*(arr[i]));
+	}
+	return 0;
+}
